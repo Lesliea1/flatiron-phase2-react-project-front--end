977cc0001ad7a4abcb43844e1f611bd04ffab037
leslie <alvarado.leslie12@yahoo.com>
2023-10-06 16:23:29 -0700

    Food list component created

--- my-react-app/src/components/Main.jsx
+++ my-react-app/src/components/Main.jsx
@@ -1,8 +1,11 @@
+import FoodList from "./FoodList"
+
 const Main = () => {
     return (
         <>
             <main>
                 <h2> Main section of our application </h2>
+                <FoodList/>
                 <p>
                     some text
                 </p>
