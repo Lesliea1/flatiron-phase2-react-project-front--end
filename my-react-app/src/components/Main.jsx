e2b399b0c536b014820c2565ef282d21242acae0
leslie <alvarado.leslie12@yahoo.com>
2023-10-07 15:08:30 -0700

    updated inline css style

--- my-react-app/src/components/Main.jsx
+++ my-react-app/src/components/Main.jsx
@@ -3,9 +3,14 @@ import HolidayList from "./HolidayList"
 const Main = () => {
     return (
         <>
-            <main>
-                <h2> Swedish </h2>
-                <p>
+        
+            <main id="some-id1" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                <h2>Sweden Local Traditions</h2>
+                <img src="https://i0.wp.com/scandification.com/wp-content/uploads/2021/09/Swedish-Holidays-1-scaled.jpg?resize=1155%2C770&ssl=1"
+                    alt="Swedish dancing women" style={{ width: '280px', height: '280px' }} />
+                
+<br></br>
+                <p >
                     Swedish locals known to be cultured and festive at
                     heart. Many partipate in the numerous local festivities 
                     for the holidays, some of which can be found below. Let's 
@@ -14,8 +19,9 @@ const Main = () => {
 <br></br>
                 <HolidayList/>
                
-                
+        
             </main> 
+           
         </>
     )
 }
