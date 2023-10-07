f0f008b19d3d44c393c648a5fb866ca300e79274
leslie <alvarado.leslie12@yahoo.com>
2023-10-06 20:22:47 -0700

    added p tag and text #1

--- my-react-app/src/components/Main.jsx
+++ my-react-app/src/components/Main.jsx
@@ -1,14 +1,19 @@
-import FoodList from "./FoodList"
+import HolidayList from "./HolidayList"
 
 const Main = () => {
     return (
         <>
             <main>
-                <h2> Main section of our application </h2>
-                <FoodList/>
+                <h2> Swedish </h2>
                 <p>
-                    some text
+                    Swedish locals known to be cultured and festive at
+                    heart. Many partipate in the numerous local festivities 
+                    for the holidays, some of which can be found below. Let's 
+                    explore!
                 </p>
+<br></br>
+                <HolidayList/>
+               
                 
             </main> 
         </>
