00a59b2a60d5e4cb352b440a7f66c1636aa15fd0
leslie <alvarado.leslie12@yahoo.com>
2023-10-07 15:07:51 -0700

    udated header nav links

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -1,22 +1,31 @@
+
+
 const Header = () => {
     return (
         <>
-            <header>
+            <header style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                 <h1> Sweden for Thought! </h1>
-                <nav>
+
+<br></br>
+<br></br>
+                
+                    <img src="https://wallpapercave.com/wp/wp2025142.jpg" alt="City life Sweden" style={{ width: '880px', height: '480px'}} /> 
+
+<br></br>
+<br></br>
+                    <nav>
                     <u1>
-                        <li><a href="#some-id1">
-                            Some Link Title 1 </a></li>
-                        <li><a href="#some-id2">
-                            Some Link Title 2 </a></li>
-                        <li><a href="#some-id3">
-                            Some Link Title 3 </a></li>
+                            <li><a href="#some-id1"> Sweden Local Traditions </a></li>
+                            <li><a href="#some-id2"> Holidays Celebrated in Sweden </a></li>
+                            <li><a href="#some-id3"> Sweden Capital of Festivities </a></li>
                     </u1>
                 </nav>
             </header> 
+<br></br>
+<br></br>
         </>
     )
 }
 
-
+      
 export default Header;
