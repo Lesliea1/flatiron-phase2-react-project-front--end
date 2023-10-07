07c03818c6bb8165e80ddf3d7df4488833d85b9b
leslie <alvarado.leslie12@yahoo.com>
2023-10-07 15:07:34 -0700

    updated footer nav links

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -1,14 +1,17 @@
 const Footer = () => {
     return (
         <>
-            <footer>
+<br></br>
+<br></br>
+<br></br>
+            <footer style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                     <u1>
                         <li><a href="#top">
                             Go to top! </a></li>
-                        <li><a href="#" target="_blank">
-                        Some Link Title 1 </a></li>
-                        <li><a href="#" target="_blank">
-                            Some Link Title 2 </a></li>
+                        <li><a href="#some-id1">
+                        Sweden Local Traditions </a></li>
+                    <li><a href="#some-id2">
+                        Holidays Celebrated in Sweden </a></li>
                     </u1>
             </footer> 
         </>
