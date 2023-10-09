f21c1153998162f2aebc89a7d1219e3c701c6555
leslie <alvarado.leslie12@yahoo.com>
2023-10-08 17:37:58 -0700

    Added stanza p tag to main comp.

--- my-react-app/src/components/HolidayList.jsx
+++ my-react-app/src/components/HolidayList.jsx
@@ -41,7 +41,7 @@ const HolidayList = () => {
         
     return (
         <>
-        
+         
             <h2 id="some-id2">Holidays Celebrated in Sweden</h2>
             <table>
                 <thead>
@@ -58,10 +58,31 @@ const HolidayList = () => {
                     {renderedHolidays}
                 </tbody>
             </table>
+
+            <br></br>
+            <br></br>
+            <br></br>
+            
+            <h2 id="some-id3">Swedish Jul</h2>
+          
+            <img src="https://i0.wp.com/scandification.com/wp-content/uploads/2021/10/Swedish-Christmas-traditions-5-scaled.jpg?resize=1024%2C640&ssl=1"
+                alt="Swedish snack" style={{ width: '480px', height: '380px' }} /> 
+            <br></br>
+              <br></br>
                 <p>
-                    some text
-                </p>
-                
+                In Sweden, the prime holiday tradition centers
+                in the celebration of Swedish Jul, which is the Swedish version
+                Christmas. This festivity, which begins
+                about four weeks prior to Christmas,
+                starts with the lighting of the initial candle. This candle lighting commences a month-long
+                festive season filled with
+                unique beverages, festive games, and traditional snacks. Christmas in Sweden, is usually very dark and cold,
+                so the festive season is a chance to bring some
+                joy back into the people and spend important moments with family and friends.
+            </p>
+            <br></br>
+              <br></br>
+   
         </>
     )
 }
