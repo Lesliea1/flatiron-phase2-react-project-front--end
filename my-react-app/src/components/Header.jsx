d9a595e42407e9ab9f34419fc93721cc93bdec6c
leslie <alvarado.leslie12@yahoo.com>
2023-10-08 17:37:38 -0700

    Added nav link and changed image

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -5,23 +5,28 @@ const Header = () => {
         <>
             <header style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                 <h1> Sweden for Thought! </h1>
-
-<br></br>
-<br></br>
+                <br></br>
                 
-                    <img src="https://wallpapercave.com/wp/wp2025142.jpg" alt="City life Sweden" style={{ width: '880px', height: '480px'}} /> 
-
-<br></br>
-<br></br>
-                    <nav>
+                 <nav>
                     <u1>
                             <li><a href="#some-id1"> Sweden Local Traditions </a></li>
                             <li><a href="#some-id2"> Holidays Celebrated in Sweden </a></li>
-                            <li><a href="#some-id3"> Sweden Capital of Festivities </a></li>
+                        <li><a href="#some-id3"> Swedish Jul </a></li>
+                         <li><a href="#some-id4">
+                        Celebrating the Swedish Culture</a></li>
                     </u1>
                 </nav>
-            </header> 
+
+                <br></br>
+                <br></br>
+                
+                    <img src="https://wallpapercave.com/wp/wp2025142.jpg" alt="City life Sweden" style={{ width: '1180px', height: '400px'}} /> 
+
 <br></br>
+<br></br>
+                   
+            </header> 
+            <br></br>
 <br></br>
         </>
     )
