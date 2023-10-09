8ad517ff379a0768098dfa082ee39da11b2ee303
leslie <alvarado.leslie12@yahoo.com>
2023-10-08 17:37:09 -0700

    Updated paragraph and photo inlcusion

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -1,17 +1,41 @@
 const Footer = () => {
     return (
         <>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <h2 id="some-id4"> Celebrating the Swedish Culture </h2>
+            <img src="https://i0.wp.com/scandification.com/wp-content/uploads/2021/09/Swedish-Holidays-7-scaled.jpg?resize=1024%2C640&ssl=1" alt="Sweden Statue" style={{ width: '480px', height: '380px'}}/>
+<br></br>
+<br></br>
+                <p>
+                    Swedish holidays provide a special opportunity to get a peek into
+                    Swedish culture and get to know the people better. While some of the
+                    important holidays in Sweden are like ours, the Swedish natives also
+                    have their special customs that are interesting to explore.
+                    In Sweden, people take their holidays quite seriously.
+                    Public holidays come with a bunch of Swedish traditions that the locals strictly adhere to. Each day comes with its own set of things to do,
+                    special foods to enjoy, and many other exciting customs. Explore your next
+                    trip to Sweden around a festivity in mind!
+                </p>
+            </div>
 <br></br>
 <br></br>
 <br></br>
-            <footer style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <footer style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }} >
                     <u1>
-                        <li><a href="#top">
-                            Go to top! </a></li>
                         <li><a href="#some-id1">
                         Sweden Local Traditions </a></li>
                     <li><a href="#some-id2">
                         Holidays Celebrated in Sweden </a></li>
+                    <li><a href="#some-id3">
+                        Swedish Jul </a></li>
+                     <li><a href="#some-id4">
+                        Celebrating the Swedish Culture</a></li>
+                    
+                    <br></br>
+                    <br></br>
+                    
+                     <li><a href="#top">
+                            Go to top! </a></li>
                     </u1>
             </footer> 
         </>
