4774a94710e6650ba8d93ff6412f97aaa73036b0
leslie <alvarado.leslie12@yahoo.com>
2023-10-10 23:38:17 -0700

    added routes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,30 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import { useState } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Corrected 'Link'
+import About from './components/About';
+import Home from './components/Home'
+
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-      <Header />
-      <Main />
-      <Footer />
+   return (
+    <Router>
+       <div>
+         <Header />
+         <Routes>
+           <Route path="/" element={<Home />} />
+           <Route path="/about" element={<About />} />
+        </Routes>
+        <Main />
+        <Footer />
+        
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+
+export default App;
