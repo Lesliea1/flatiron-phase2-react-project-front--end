bd8d1392a997852aa82517263dd8abd5868f3975
leslie <alvarado.leslie12@yahoo.com>
2023-10-10 23:39:31 -0700

    made home component

---
+++ my-react-app/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import '../styles/Home.css';
+
+
+const Home = () => (
+  <div className='home-container'>
+        <h1 className='home-title'>Table of Contents:</h1>
+        <p className='home-text'>
+
+    <nav className="main-nav">
+        <ul>
+          <li><a href="#some-id1">Sweden Local Traditions</a></li>
+          <li><a href="#some-id2">Holidays Celebrated in Sweden</a></li>
+          <li><a href="#some-id3">Swedish Jul</a></li>
+          <li><a href="#some-id4">Celebrating the Swedish Culture</a></li>
+        </ul>
+            </nav>
+
+        </p>
+  </div>
+);
+
+
+export default Home;
