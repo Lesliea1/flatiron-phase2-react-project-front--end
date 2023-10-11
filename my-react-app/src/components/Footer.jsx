96a122389d2b23148916135245c819be8489a55a
leslie <alvarado.leslie12@yahoo.com>
2023-10-10 23:38:45 -0700

    footer updated

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -1,12 +1,13 @@
+import '../styles/Footer.css'
+
 const Footer = () => {
     return (
         <>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-            <h2 id="some-id4"> Celebrating the Swedish Culture </h2>
-            <img src="https://i0.wp.com/scandification.com/wp-content/uploads/2021/09/Swedish-Holidays-7-scaled.jpg?resize=1024%2C640&ssl=1" alt="Sweden Statue" style={{ width: '480px', height: '380px'}}/>
-<br></br>
-<br></br>
-                <p>
+            <div className='footer-container'>
+            <h2 className='footer-title' id="some-id4">Celebrating the Swedish Culture</h2>
+                <img className='footer-image' src="https://i0.wp.com/scandification.com/wp-content/uploads/2021/09/Swedish-Holidays-7-scaled.jpg?resize=1024%2C640&ssl=1" alt="Sweden Statue" />
+                
+                <p className='footer-text'>
                     Swedish holidays provide a special opportunity to get a peek into
                     Swedish culture and get to know the people better. While some of the
                     important holidays in Sweden are like ours, the Swedish natives also
@@ -17,27 +18,19 @@ const Footer = () => {
                     trip to Sweden around a festivity in mind!
                 </p>
             </div>
-<br></br>
-<br></br>
-<br></br>
-            <footer style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }} >
-                    <u1>
-                        <li><a href="#some-id1">
-                        Sweden Local Traditions </a></li>
-                    <li><a href="#some-id2">
-                        Holidays Celebrated in Sweden </a></li>
-                    <li><a href="#some-id3">
-                        Swedish Jul </a></li>
-                     <li><a href="#some-id4">
-                        Celebrating the Swedish Culture</a></li>
-                    
-                    <br></br>
-                    <br></br>
-                    
-                     <li><a href="#top">
-                            Go to top! </a></li>
-                    </u1>
-            </footer> 
+             
+            <nav className='footer-navLinks'>
+                <div className="footer-line" ></div>
+                    <ul>
+                        <li><a href="#some-id1">Sweden Local Traditions</a></li>
+                        <li><a href="#some-id2">Holidays Celebrated in Sweden</a></li>
+                        <li><a href="#some-id3">Swedish Jul</a></li>
+                        <li><a href="#some-id4">Celebrating the Swedish Culture</a></li>
+                        <li><a href="#top">Go to top! </a></li>
+                    </ul>
+            </nav>
+ 
+            <p className='footer-text2'>Copyright Avocado 2023</p>
         </>
     )
 }
