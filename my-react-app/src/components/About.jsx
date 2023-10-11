9b46232ce79c02ffb3d511c6c15eb9eab87ed648
leslie <alvarado.leslie12@yahoo.com>
2023-10-10 23:38:30 -0700

    made about component

---
+++ my-react-app/src/components/About.jsx
@@ -0,0 +1,17 @@
+import '../styles/About.css';
+
+
+const About = () => {
+  return (
+    <div className='about-container'>
+      <h1>About Us</h1>
+      <p className='about-text'>We are a Swedish culture website! Encouraging visitors to
+        book their next flight around the native holidays that make
+        Sweden known for their year-round joy!
+      </p>
+    </div>
+  );
+}
+
+
+export default About;
