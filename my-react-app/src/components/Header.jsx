3577055cf42d7b27cf5975c4e1d4d17c18fa5b9d
leslie <alvarado.leslie12@yahoo.com>
2023-10-10 23:39:01 -0700

    made header css file

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -1,36 +1,33 @@
+import '../styles/Header.css';
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
-    return (
-        <>
-            <header style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                <h1> Sweden for Thought! </h1>
-                <br></br>
-                
-                 <nav>
-                    <u1>
-                            <li><a href="#some-id1"> Sweden Local Traditions </a></li>
-                            <li><a href="#some-id2"> Holidays Celebrated in Sweden </a></li>
-                        <li><a href="#some-id3"> Swedish Jul </a></li>
-                         <li><a href="#some-id4">
-                        Celebrating the Swedish Culture</a></li>
-                    </u1>
-                </nav>
 
-                <br></br>
-                <br></br>
-                
-                    <img src="https://wallpapercave.com/wp/wp2025142.jpg" alt="City life Sweden" style={{ width: '1180px', height: '400px'}} /> 
+  return (
+    <header className="header-container">
+          <h1 className="header-title">Sweden for Thought!</h1>
+          
+           <img
+        className="header-image"
+        src="https://wallpapercave.com/wp/wp2025142.jpg"
+        alt="City life Sweden"
+          />
+          
+ <nav className='home-nav'>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+           <li>
+           <Link to="/about">About</Link>
+          </li>
+        </ul>
+</nav>
+           <div className="header-line"></div>
+          
+    </header>
+  );
+};
 
-<br></br>
-<br></br>
-                   
-            </header> 
-            <br></br>
-<br></br>
-        </>
-    )
-}
-
-      
 export default Header;
